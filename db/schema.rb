715f82af4b62475a81f0de5223037a933dd53c8b
Marcos Vives <marcoshvives@gmail.com>
2021-08-05 23:33:17 +0200

    working postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2021_06_25_102217) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "adminpack"
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string "event_type"
     t.date "event_date"
